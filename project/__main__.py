10cd1db6d7043bcb05c05f6371e63283c1a78866
Ethan Tolson <tol18009@byui.edu>
2021-11-15 12:10:54 -0800

    Added basic sprites, made sure that program will run, still needs movement functionality, and sprite functionailty

--- project/__main__.py
+++ project/__main__.py
@@ -6,6 +6,7 @@ def main():
     window = arcade.Window(const.SCREEN_WIDTH, const.SCREEN_WIDTH, const.SCREEN_TITLE, resizable = True, fullscreen = True)
     arcade.set_background_color(arcade.color.ARSENIC)
     gameView = Director()
+    gameView.setup()
     window.show_view(gameView)
     arcade.run()
 
