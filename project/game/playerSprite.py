399cbffd14ed00a105f90b0bc959849304534c98
JWilderman <kaodremora@outlook.com>
2021-11-18 16:09:28 -0500

    Work on playerSprite class

--- project/game/playerSprite.py
+++ project/game/playerSprite.py
@@ -4,9 +4,16 @@ class PlayerSprite(arcade.Sprite):
 
     def __init__(self, filename, scaling):
         super().__init__(filename, scaling)
+        self.player_hp = 3
+        self.enemySprites = None
+        self.collision = False
 
-    def update(self):
+    def update(self, enemySpriteList):
         super().update()
+        self.enemySprites = enemySpriteList
+        self.collision = arcade.check_for_collision_with_list(enemySpriteList)
+        if(self.collision == True):
+           self.onHit()
 
     def onHit(self):
-        pass
+        self.player_hp -= 1
