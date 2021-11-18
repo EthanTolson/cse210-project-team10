030f7166b0fdf9fe01015698b9e9d17665c2ecac
Ethan Tolson <tol18009@byui.edu>
2021-11-18 15:46:45 -0800

    I dont even rememb er but it is important

--- project/game/constants.py
+++ project/game/constants.py
@@ -2,6 +2,10 @@ import os
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+HEALTHBAR_WIDTH = 50
+HEALTHBAR_HEIGHT = 6
+HEALTHBAR_OFFSET_Y = -50
+HEALTH_NUMBER_OFFSET_Y = -50
 SCALING = 1.5
 RESOURCE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/resources/"
 SCREEN_TITLE = "Zombie Shooter"
--- project/game/director.py
+++ project/game/director.py
@@ -6,12 +6,6 @@ from game.playerSprite import PlayerSprite
 from game.enemysprite import EnemySprite
 from game.projectile import ProjectileSprite
 
-HEALTHBAR_WIDTH = 50
-HEALTHBAR_HEIGHT = 6
-HEALTHBAR_OFFSET_Y = -50
-
-HEALTH_NUMBER_OFFSET_Y = -50
-
 class Director(arcade.View):
     def __init__(self):
         super().__init__()
@@ -36,26 +30,30 @@ class Director(arcade.View):
         for enemySprite in self.enemySprites:            
             self.drawHealthBars(enemySprite)
         self.drawHealthBars(self.playerSprite[0])
-        arcade.draw_text(f"Score: {self.score}", self.player.center_x - self.window.width/2 + 10, self.player.center_y - self.window.height/2 + 20, arcade.color.WHITE, 14)
+        arcade.draw_text(f"Level: {self.level} | Score: {self.score} | Zombies Left: {len(self.enemySprites)}",
+         self.player.center_x - self.window.width/2 + 10, 
+         self.player.center_y - self.window.height/2 + 20, 
+         arcade.color.WHITE, 14)
         self.camera_sprites.use()
         self.allSprites.draw()
         
     def drawHealthBars(self, sprite):
-        health_width = HEALTHBAR_WIDTH * (sprite.getHealth() / sprite.getMaxHealth())
+        health_width = const.HEALTHBAR_WIDTH * (sprite.getHealth() / sprite.getMaxHealth())
         if sprite.getHealth() < sprite.getMaxHealth():
             arcade.draw_rectangle_filled(center_x = sprite.center_x,
-                    center_y=sprite.center_y + HEALTHBAR_OFFSET_Y,
-                    width=HEALTHBAR_WIDTH,
+                    center_y=sprite.center_y + const.HEALTHBAR_OFFSET_Y,
+                    width=const.HEALTHBAR_WIDTH,
                     height=3,
                     color=arcade.color.RED)
-        arcade.draw_rectangle_filled(center_x=sprite.center_x - 0.5 * (HEALTHBAR_WIDTH - health_width),
-                    center_y=sprite.center_y + HEALTHBAR_OFFSET_Y,
+        arcade.draw_rectangle_filled(center_x=sprite.center_x - 0.5 * \
+                    (const.HEALTHBAR_WIDTH - health_width),
+                    center_y=sprite.center_y + const.HEALTHBAR_OFFSET_Y,
                     width=health_width,
-                    height=HEALTHBAR_HEIGHT,
+                    height=const.HEALTHBAR_HEIGHT,
                     color=arcade.color.GREEN)
         arcade.draw_text(f"{sprite.getHealth()}/{sprite.getMaxHealth()}",
                     start_x=sprite.center_x,
-                    start_y=sprite.center_y + HEALTH_NUMBER_OFFSET_Y,
+                    start_y=sprite.center_y + const.HEALTH_NUMBER_OFFSET_Y,
                     font_size=12,
                     color=arcade.color.WHITE, anchor_x="center")
 
@@ -89,11 +87,19 @@ class Director(arcade.View):
     def spawnEnemies(self):
         if self.level <= 10:
             for i in range(0, 10 * self.level):
-                self.enemy = EnemySprite(const.RESOURCE_PATH + "zombiePNG.png", const.SCALING) 
-                self.enemy.center_x = random.randint(0, 1000)
-                self.enemy.center_y = random.randint(0, 1000)
-                self.enemySprites.append(self.enemy)
-                self.allSprites.append(self.enemy)
+                if i % 2:
+                    self.enemy = EnemySprite(const.RESOURCE_PATH + "zombiePNG.png", const.SCALING) 
+                    self.enemy.center_x = random.randint(0, 1000)
+                    self.enemy.center_y = random.randrange(0, 3001, 3000)
+                    self.enemySprites.append(self.enemy)
+                    self.allSprites.append(self.enemy)
+                else:
+                    self.enemy = EnemySprite(const.RESOURCE_PATH + "zombiePNG.png", const.SCALING) 
+                    self.enemy.center_x = random.randrange(0, 3001, 3000)
+                    self.enemy.center_y = random.randint(0, 1000)
+                    self.enemySprites.append(self.enemy)
+                    self.allSprites.append(self.enemy)
+                
 
     def spawnProjectiles(self, x, y):
         self.projectile = ProjectileSprite(const.RESOURCE_PATH + "projectilePNG.png", 1/8 * const.SCALING)
@@ -108,8 +114,8 @@ class Director(arcade.View):
        
     def setup(self):
         self.player = PlayerSprite(const.RESOURCE_PATH + "playerPNG.png", const.SCALING) 
-        self.player.center_x = 100
-        self.player.center_y = 200
+        self.player.center_x = 1500
+        self.player.center_y = 1500
         self.score = 0
         self.playerSprite.append(self.player)     
         self.allSprites.append(self.player)
