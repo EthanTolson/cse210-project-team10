d0fd2c33c180934cf438875e8e6f330845e5c951
Stephen Nielsen <smnielsen256@byui.edu>
2021-11-18 14:09:00 -0700

    started projectile.py

--- project/game/projectile.py
+++ project/game/projectile.py
@@ -1,11 +1,29 @@
 import arcade
+import playerSprite
 
 class ProjectileSprite(arcade.Sprite):
-    def __init__(self, filename, scaling):
+    def __init__(self, filename, scaling, spawn_x, spawn_y):
         super().__init__(filename, scaling)
+
+        self.sprite = arcade.Sprite("", scaling)
+
+        self.destinaion = ""
+        self.speed = 100 # change as needed, 100 is not a special number
+
+        #vector is made from source, destination, and speed.
+        self.vector = super().velocity
 
     def update(self):
         super().__init__()
 
+        # moves according to vecto
+
+        # executes onHit() if it hits something
+        if self.collides_with_sprite("""enemy"""):
+            self.onHit()
+
     def onHit(self):
-        pass
+        """
+        deletes projectile
+        """
+        self.remove_from_sprite_lists()
