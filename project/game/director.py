b5102eab7ddc62734cc2d6f08cb2c1b531f08bba
Ethan Tolson <tol18009@byui.edu>
2021-11-18 12:25:36 -0800

    Added basic movement for player

--- project/game/director.py
+++ project/game/director.py
@@ -11,12 +11,15 @@ class Director(arcade.View):
         self.playerSprite = arcade.SpriteList()
         self.enemySprites = arcade.SpriteList()
         self.allSprites = arcade.SpriteList()
+        self.camera_sprites = arcade.Camera(self.window.width, self.window.height)
 
     def on_update(self, delta_time: float):
         self.allSprites.update()
+        self.scroll_to_player()
 
     def on_draw(self):
         arcade.start_render()
+        self.camera_sprites.use()
         self.allSprites.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
@@ -25,8 +28,26 @@ class Director(arcade.View):
         elif symbol == arcade.key.ESCAPE:
             arcade.close_window()
 
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+
+            x = x + self.player.center_x - self.window.width/2
+            y = y + self.player.center_y - self.window.height/2
+
+            self.player.change_x = 7 * ((x- self.player.center_x ) / math.sqrt((x-self.player.center_x)**2 + (y- self.player.center_y)**2))
+            self.player.change_y = 7 * ((y- self.player.center_y ) / math.sqrt((x-self.player.center_x)**2 + (y- self.player.center_y)**2))
+
+
+    def scroll_to_player(self):
+        position = self.player.center_x - self.window.width / 2, \
+            self.player.center_y - self.window.height / 2
+        self.camera_sprites.move_to(position, .75)
+
+    def on_resize(self, width: int, height: int):
+        self.camera_sprites.resize(int(width), int(height))
+
     def setup(self):
-        self.player = PlayerSprite(const.RESOURCE_PATH + "playerPNG.png", const.SCALING)
+        self.player = PlayerSprite(const.RESOURCE_PATH + "playerPNG.png", const.SCALING) 
         self.player.center_x = 100
         self.player.center_y = 200
         self.playerSprite.append(self.player)
