aaf6b2ac8d24efe0494e09ae2d37631a8e6ac075
Ethan Tolson <tol18009@byui.edu>
2021-11-29 14:57:26 -0800

    Added basic code for shooter enemy classes needs to be finished before used

---
+++ project/game/shooterSprite.py
@@ -0,0 +1,47 @@
+import arcade
+import math
+"""
+EnemySprite Class:
+Subclass of Arcade Sprite. Used for Enemies.
+
+Attributes:
+    hitPoints (INT) The current health of enemy
+    player (PlayerSprite) Sprite object for player
+"""
+
+class ShooterSprite(arcade.Sprite):
+    def __init__(self, filename, scaling):
+        super().__init__(filename, scaling)
+        self.hitPoints = 3
+        self.player = None
+        self.damage = 2
+        self.lastHit = 99999999
+        
+    def update(self):
+        super().update()
+        self.change_x = 6 * (( self.player.center_x - self.center_x ) / math.sqrt((self.center_x-self.player.center_x)**2 + (self.center_y- self.player.center_y)**2))
+        self.change_y = 6 * (( self.player.center_y - self.center_y ) / math.sqrt((self.center_x-self.player.center_x)**2 + (self.center_y- self.player.center_y)**2))
+        self.angle = math.atan2(self.player.center_y - self.center_y, self.player.center_x - self.center_x) * 180 / math.pi
+        if self.hitPoints <= 0:
+            self.remove_from_sprite_lists()
+
+    def getHealth(self):
+        return self.hitPoints
+
+    def getMaxHealth(self):
+        return 3
+    
+    def getDamage(self):
+        return self.damage
+
+    def onHit(self):
+        self.hitPoints -= 1
+    
+    def setPlayer(self, player):
+        self.player = player
+
+    def getLastHit(self):
+        return self.lastHit
+
+    def setLastHit(self, hit):
+        self.lastHit = hit
