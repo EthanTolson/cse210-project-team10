fb1b0acb4ccdae28c4c9cb8f278b7ca4e48ca63e
Austinsways <eld20009@byui.edu>
2021-12-02 14:33:22 -0700

    added code for startScreen function and altered main to call startscreen over director

--- project/__main__.py
+++ project/__main__.py
@@ -1,12 +1,11 @@
 import arcade
 from game import constants as const
-from game.director import Director
+from game.startScreen import StartScreen
 
 def main():
     window = arcade.Window(const.SCREEN_WIDTH, const.SCREEN_WIDTH, const.SCREEN_TITLE, resizable = True, fullscreen = True)
     arcade.set_background_color(arcade.color.ARSENIC)
-    gameView = Director()
-    gameView.setup()
+    gameView = StartScreen()
     window.show_view(gameView)
     arcade.run()
             
