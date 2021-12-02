5f45547e678e035ea239055721135040c2d0fdfb
miskindr <91636488+miskindr@users.noreply.github.com>
2021-12-02 14:35:05 -0700

    clean up game over

--- project/game/endScreen.py
+++ project/game/endScreen.py
@@ -1,19 +1,6 @@
 import arcade
 
 class EndScreen(arcade.View):
-    # def constant(self):
-    #     BLACK = (0, 0, 0)
-    #     RED = (255, 0, 0)
-    #     GREEN = (0, 255, 0)
-    #     BLUE = (0, 0, 255)
-    #     YELLOW = (255, 255, 0)
-    #     WHITE = (255, 255, 255)
-    #     BLACK = (0, 0, 0)
-    #     RED = (255, 0, 0)
-    #     GREEN = (0, 255, 0)
-    #     BLUE = (0, 0, 255)
-    #     YELLOW = (255, 255, 0)
-
     def __init__(self):
         super().__init__()
     
