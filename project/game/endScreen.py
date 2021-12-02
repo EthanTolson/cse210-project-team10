081669f874fa0b72bc5c2f8ebba7f52534a26a45
Ethan Tolson <tol18009@byui.edu>
2021-12-02 13:49:21 -0800

    Quick fix to end screen

--- project/game/endScreen.py
+++ project/game/endScreen.py
@@ -1,30 +1,33 @@
 import arcade
+from game import constants as const
 
 class EndScreen(arcade.View):
     def __init__(self):
         super().__init__()
     
-    def game_over(self):
-        WHITE = (255, 255, 255)
-        WIDTH = 800
-        HEIGHT = 600
+    def on_draw(self):
+        
+        # if not self.running:
+        #     return
+        #self.screen.fill(255, 0, 0)
+        arcade.start_render()
+        # arcade.draw_text("GAME OVER", 48, 100, arcade.color.WHITE, const.SCREEN_WIDTH / 2, const.SCREEN_HEIGHT/ 4)
+        # arcade.draw_text(f"Score: {str(100)}", 22, 200, arcade.color.WHITE, const.SCREEN_WIDTH / 2, const.SCREEN_HEIGHT/ 2)
+        arcade.draw_text("Press a key to play again", 22, 300, arcade.color.WHITE, const.SCREEN_WIDTH / 2)#, const.SCREEN_HEIGHT * 3/4)
+        # arcade.display.flip()
+        # # self.wait_for_key()
 
-        if not self.running:
-            return
-        self.screen.fill(255, 0, 0)
-        self.draw_text("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
-        self.draw_text("Score: " + str(self.score), 22, WHITE, WIDTH / 2, HEIGHT / 2)
-        self.draw_text("Press a key to play again", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
-        arcade.display.flip()
-        self.wait_for_key()
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.ESCAPE:
+            arcade.close_window()
     
-    def wait_for_key(self):
-        waiting = True
-        while waiting:
-            self.clock.tick(60)
-            for event in arcade.event.get():
-                if event.type == arcade.QUIT:
-                    waiting = False
-                    self.running = False
-                if event.type == arcade.KEYUP:
-                    waiting = False
+    # def wait_for_key(self):
+    #     waiting = True
+    #     while waiting:
+    #         self.clock.tick(60)
+    #         for event in arcade.event.get():
+    #             if event.type == arcade.QUIT:
+    #                 waiting = False
+    #                 self.running = False
+    #             if event.type == arcade.KEYUP:
+    #                 waiting = False
