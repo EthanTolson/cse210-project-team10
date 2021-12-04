faf4822a8669a711fa0628604e786e608b82f3b1
Ethan Tolson <tol18009@byui.edu>
2021-12-03 19:36:43 -0800

    Hud upgrades

--- project/game/hud.py
+++ project/game/hud.py
@@ -17,7 +17,13 @@ class drawHUD():
             r = 255
         arcade.draw_xywh_rectangle_filled(director.player.center_x - director.window.width / 2 + 25, director.player.center_y  - 200, 125, 400, arcade.color.ARSENIC)
         arcade.draw_lrwh_rectangle_textured(director.player.center_x - director.window.width / 2 + 50, director.player.center_y  + 105, 75, 75, arcade.load_texture(const.RESOURCE_PATH + "qhudPNG.png"))
+        arcade.draw_circle_filled(director.player.center_x - director.window.width / 2 + 125, director.player.center_y  + 180, 14, arcade.color.GENERIC_VIRIDIAN)
+        arcade.draw_circle_filled(director.player.center_x - director.window.width / 2 + 50, director.player.center_y  + 180, 14, arcade.color.GENERIC_VIRIDIAN)
+        arcade.draw_text(f"{director.q.shotsLeft[1]}", director.player.center_x - director.window.width / 2 + 125, director.player.center_y  + 180, arcade.color.SMOKY_BLACK, 14, anchor_x = "center", anchor_y = "center")
+        arcade.draw_text(f"{director.q.shotsLeft[0]}", director.player.center_x - director.window.width / 2 + 50, director.player.center_y  + 180, arcade.color.SMOKY_BLACK, 14, anchor_x = "center", anchor_y = "center")
         arcade.draw_lrwh_rectangle_textured(director.player.center_x - director.window.width / 2 + 50, director.player.center_y  + 10, 75, 75, arcade.load_texture(const.RESOURCE_PATH + "whudPNG.png"), alpha = w)
         arcade.draw_lrwh_rectangle_textured(director.player.center_x - director.window.width / 2 + 50, director.player.center_y  - 85, 75, 75, arcade.load_texture(const.RESOURCE_PATH + "ehudPNG.png"), alpha = e)
+        arcade.draw_circle_filled(director.player.center_x - director.window.width / 2 + 125, director.player.center_y  + 85, 14, arcade.color.GENERIC_VIRIDIAN)
+        arcade.draw_text(f"{director.grenade[1]}", director.player.center_x - director.window.width / 2 + 125, director.player.center_y  + 85, arcade.color.SMOKY_BLACK, 14, anchor_x = "center", anchor_y = "center")
         arcade.draw_lrwh_rectangle_textured(director.player.center_x - director.window.width / 2 + 50, director.player.center_y  - 180, 75, 75, arcade.load_texture(const.RESOURCE_PATH + "rhudPNG.png"), alpha = r)
         
